4e9f9b1425633d635c7e9d4d239c3bdc5b089805
Sonilalchavhan <chavhansonilal@gmail.com>
2026-01-22 21:17:41 +0530

    US-07:Full Profile page added

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebApplication.Controllers
             return View();
         }
 
+        public IActionResult SonilalChavhan()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
