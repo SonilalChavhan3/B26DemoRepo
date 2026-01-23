d60f8b237b8f502bb6ed69881648f948f2d341fc
gundbharti <140239748+gundbharti@users.noreply.github.com>
2026-01-23 22:29:14 +0530

    gundbharti -01:personal details added (#22)
    Co-authored-by: Sonilal Chavhan <132873146+SonilalChavhan3@users.noreply.github.com>

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace WebApplication.Controllers
             return View();
         }
 
+        public IActionResult BhartiPawar()
+        {
+            return View();
+        }
+
         public IActionResult SonilalChavhan()
         {
             return View();
